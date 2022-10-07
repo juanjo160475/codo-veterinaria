43c5cbe3945dbf3b71480ca01893da4844eb0530
juanjo <juanjo>
2022-10-07 16:11:59 -0300

    menu amburguesa

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded',(event) =>{
+const menuBtn = document.getElementById ('menu')
+const navegar = document.getElementById('ul-nav')
+const body = document.querySelector('body')
+
+menuBtn.addEventListener("click",(event) =>{
+menuBtn.classList.toggle('salir')
+navegar.classList.toggle('visible')
+body.classList.toggle('no-scroll')
+})
+})
