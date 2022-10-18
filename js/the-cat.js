fff0d64587350a9a3cdbe5ac266c212f057513fc
juanjo <juanjo>
2022-10-18 10:38:43 -0300

    agreando descripciones en gatos

--- js/the-cat.js
+++ js/the-cat.js
@@ -1,16 +1,17 @@
 //change the limit to however many images to use
-const url = `https://api.thecatapi.com/v1/images/search?limit=8&has_breeds=1`;
+const url = `https://api.thecatapi.com/v1/images/search?limit=12&has_breeds=1`;
 const api_key = "live_S7y8UFk1mFSEPH1jUkZx0L3mVDx1VunSy5HjXa2RsDYU88rYlRxhH3qFv0LLb6ww"
 const div_imagen= document.getElementById("img-elegida")
 const fila = document.getElementById('carrousel-imagenes')
 const flechaIzquierda = document.getElementById('flecha-izquierda')
 const flechaDerecha = document.getElementById('flecha-derecha')
-
+let imagesData;
 
 
 
 
 //cargando data
+
 fetch(url,{headers: {
       'x-api-key': api_key
     }})
@@ -18,7 +19,7 @@ fetch(url,{headers: {
   return response.json();
 })
 .then((data) => {
-  let imagesData = data;
+  imagesData = data
   let gridCell
   cargarFlechas()
   imagesData.map(function(imageData,index) {
@@ -30,16 +31,17 @@ fetch(url,{headers: {
     //use the url from the image object
      image.src = `${imageData.url}`
      hov.appendChild(image)
-     
     gridCell.appendChild(hov)
      document.getElementById('grid').appendChild(gridCell)
     // seleccionarImagen(imageData.url)
     // llenarListas(imageData.breeds[0])
     if (index==0) {
       primeraImagen(image)
+      llenarListas(buscarCualidades( `${imageData.url}`))
     }
     
     });
+
     imagenListener()
 })
 .catch(function(error) {
@@ -67,9 +69,9 @@ function imagenListener() {
    });
 
 element.addEventListener('click', (e) => {
- console.log(e.target)
- seleccionarImagen(e.target.src)
  
+ seleccionarImagen(e.target.src)
+   llenarListas(buscarCualidades(e.target.src))
 })
 
  });
@@ -77,6 +79,10 @@ element.addEventListener('click', (e) => {
 function cargarFlechas(){
   flechaDerecha.addEventListener('click', () => {
     fila.scrollLeft += fila.offsetWidth;
+    
+    if (fila.scrollLeft == 872){
+      
+    }
   });
   flechaIzquierda.addEventListener('click', () => {
     fila.scrollLeft -= fila.offsetWidth;
@@ -87,7 +93,6 @@ function cargarFlechas(){
 
 function primeraImagen(imagen) {
 const miClon = imagen.cloneNode(true)
- const miImagen = document.createElement("img")
    div_imagen.appendChild(miClon)
   
 }
@@ -100,19 +105,45 @@ function seleccionarImagen(imagen) {
   `
 }
 
+function buscarCualidades(urlImagen) {
+  let aux
+  imagesData.forEach(element => {
+    if (element.url == urlImagen) {
+    //  console.log("raza "+ element.breeds[0].name)
+     aux= element.breeds[0]
+    }
+
+  });
+  return aux
+}
+function dibujaCualidades (num){
+ let aux=""
+ if(num>0){
+ for (let index = 0; index < num; index++) {
+   aux += `<img src="imagenes/inicio.ico" alt="">`
+ }
+}else{
+  aux=  `<span>sin datos</span>`
+}
+ return aux
+ }
+
 function llenarListas(cualidades) {
+  
   const lista = document.getElementById("id-ul")
   lista.innerHTML=`
-  <li>Raza : ${cualidades.name} </li>
-  <li>Raza : ${cualidades.wikipedia_url} </li>
-  <li>Inteligencia : ${cualidades.intelligence} </li>
-  <li>Adaptibilidad : ${cualidades.adaptability} </li>
-  <li>Aseo : ${cualidades.grooming} </li>
-  <li>Problemas de salud : ${cualidades.healh_issues} </li>
-  <li>Nivel de afecto : ${cualidades.affection_level} </li>
-  <li>Nivel de energia : ${cualidades.energy_level} </li>
-  <li>Amistad con niños : ${cualidades.child_friendly} </li>
-  <li>Amistad con perros : ${cualidades.dog_friendly} </li>
-  <li>Amistad con extraños : ${cualidades.extranger_friendly} </li>
+  <li>Raza : ${dibujaCualidades (cualidades.name)} </li>
+  <li>Inteligencia : ${dibujaCualidades (cualidades.intelligence)} </li>
+  <li>Adaptibilidad :  ${dibujaCualidades (cualidades.adaptability)} </li>
+  <li>Aseo : ${dibujaCualidades (cualidades.grooming)} </li>
+  <li>Problemas de salud : ${dibujaCualidades (cualidades.healh_issues)} </li>
+  <li>Nivel de afecto : ${dibujaCualidades (cualidades.affection_level)} </li>
+  <li>Nivel de energia : ${dibujaCualidades (cualidades.energy_level)} </li>
+  <li>Amistad con niños : ${dibujaCualidades (cualidades.child_friendly)} </li>
+  <li>Amistad con perros : ${dibujaCualidades (cualidades.dog_friendly)} </li>
+  <li>Amistad con extraños : ${dibujaCualidades (cualidades.extranger_friendly)} </li>
+  <li>wikipedia : <a href="${cualidades.wikipedia_url}" target="_blank"> Ver en Wikipedia </a> </li>
   `
 }
+
+
